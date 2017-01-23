abb5a90dbc8db1262b6dfd9d3f2f7f1124730285
beac0n <mcschempp@gmx.de>
2017-01-23 13:04:47 +0100

    update tests

--- src/components/AddInput.test.js
+++ src/components/AddInput.test.js
@@ -1,8 +1,29 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {mount} from 'enzyme'
 import {AddInput} from './AddInput'
 
-it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<AddInput />, div)
-});
+describe('<AddInput />', () => {
+    it('renders without crashing', () => {
+        mount(<AddInput />)
+    })
+
+    it('should update the name', () => {
+        const wrapper = mount(<AddInput />)
+
+        const testChangeData = 'TestChangeData'
+
+        wrapper.find('Input').simulate('change', {target: {value: testChangeData}})
+        expect(wrapper.find('Input').prop('value')).toBe(testChangeData)
+    })
+
+    it('should update the name', () => {
+        const mockFunction = jest.fn()
+        const wrapper = mount(<AddInput addIndexItem={mockFunction}/>)
+
+        wrapper.find('Input').simulate('change', {target: {value: 'TestChangeData'}})
+        wrapper.find('InputGroupButton').find('button').simulate('click')
+
+        expect(mockFunction.mock.calls.length).toBe(1)
+    })
+})
+
