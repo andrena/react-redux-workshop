0c584d2a6b091408c914da7c7994f41a88be87c8
beac0n <mcschempp@gmx.de>
2017-01-23 12:34:44 +0100

    connect AddInput to store

--- src/components/AddInput.js
+++ src/components/AddInput.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import {InputGroup, InputGroupButton, Input} from 'reactstrap'
+import {actions} from '../redux/actions'
 
-export default () => (
-    <InputGroup>
-        <Input placeholder="Bitte geben Sie einen Begriff ein"/>
-        <InputGroupButton>Hinzufügen</InputGroupButton>
-    </InputGroup>
+export class AddInput extends Component {
+    constructor(props) {
+        super(props)
+
+        this.updateName = this.updateName.bind(this)
+        this.save = this.save.bind(this)
+        this.state = {currentName: ''}
+    }
+
+    updateName(currentName) {
+        this.setState({currentName})
+    }
+
+    save() {
+        const {addIndexItem} = this.props
+        const {currentName} = this.state
+
+        addIndexItem(currentName)
+        this.setState({currentName: ''})
+    }
+
+    render() {
+        return (
+            <InputGroup>
+                <Input
+                    value={this.state.currentName}
+                    onChange={({target}) => this.updateName(target.value)}
+                    placeholder="Bitte geben Sie einen Begriff ein"
+                />
+                <InputGroupButton onClick={this.save}>Hinzufügen</InputGroupButton>
+            </InputGroup>
+        )
+    }
+}
+
+
+const mapStateToProps = (state) => (
+    {}
 )
+const mapActionsToProps = {
+    addIndexItem: actions.addIndexItem
+}
+
+
+export default connect(mapStateToProps, mapActionsToProps)(AddInput)
