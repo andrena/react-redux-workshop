122ea62eadc263aee15bdccb2f20152bb50f91d0
beac0n <mcschempp@gmx.de>
2017-01-23 13:45:27 +0100

    connect SearchInput with redux store

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {InputGroup, InputGroupButton, Input} from 'reactstrap'
+import {actions} from '../redux/actions'
 
-export default () => (
+export const SearchInput = ({currentSearch, search}) => (
     <InputGroup>
-        <InputGroupButton>Suchen</InputGroupButton>
+        <InputGroupButton onClick={({target}) => search(target.value)}>
+            Suchen
+        </InputGroupButton>
         <Input placeholder="Bitte geben Sie einen Suchbegriff ein"/>
     </InputGroup>
 )
+
+const mapStateToProps = (state) => (
+    {currentSearch: state.search}
+)
+const mapActionsToProps = {
+    search: actions.searchIndexItem
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(SearchInput)
--- src/components/SearchInput.test.js
+++ src/components/SearchInput.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchInput from './SearchInput';
+import {SearchInput} from './SearchInput';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
