10bcb5abc60d3cae640d090fa4fc5f1b279af46c
beac0n <mcschempp@gmx.de>
2017-01-23 13:24:22 +0100

    update reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,9 @@
 import {actionTypes} from './actions'
 
-export default (state, action) => {
+export default (state = {list: []}, action) => {
     switch (action.type) {
         case actionTypes.ADD_INDEX_ITEM:
-            return state
+            return Object.assign({}, state, {list: [...state.list, action.name]})
         case actionTypes.SEARCH_INDEX_ITEM:
             return state
         default:
