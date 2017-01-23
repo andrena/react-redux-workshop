14c57db3935983f3b3f35c1d0211f8db0808fe43
beac0n <mcschempp@gmx.de>
2017-01-23 10:49:03 +0100

    add jumbotron with description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import {Container, Row, Col} from 'reactstrap'
+import {Container, Row, Col, Jumbotron} from 'reactstrap'
 
 // checkout http://reactstrap.github.io/components/ to see how reactstrap components work
 
 export default () => (
     <Container>
+        <Jumbotron>
+            <h1 className="display-3">Navigatorentreffen Register</h1>
+            <p className="lead">
+                Diese kleine Anwendung stellt ein Register zur verfügung, welches man selbst befüllen kann.
+            </p>
+        </Jumbotron>
         <Row>Search Box</Row>
         <Row>Add new Index Item</Row>
         <Row>Index List</Row>
