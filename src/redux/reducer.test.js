5678bb43b6748b9f4e4da6f0a4e236c10c6cd3d6
beac0n <mcschempp@gmx.de>
2017-01-23 13:24:28 +0100

    update tests

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -3,7 +3,7 @@ import {actionTypes} from './actions'
 
 describe('reducer', () => {
     it('should return the correct default state', () => {
-        const state = {testProp: 'testProp'}
+        const state = {}
 
         const reducedState = reducer(state, {})
 
@@ -11,17 +11,20 @@ describe('reducer', () => {
     })
 
     it('should return the correct state for action type ' + actionTypes.ADD_INDEX_ITEM, () => {
-        const state = {type: actionTypes.ADD_INDEX_ITEM}
+        const testItem = 'TestItem';
+        const action = {type: actionTypes.ADD_INDEX_ITEM, name: testItem}
 
-        const reducedState = reducer(state, {})
+        const reducedState = reducer(undefined, action)
 
-        expect(reducedState).toBe(state)
+        expect(reducedState.list.length).toBe(1)
+        expect(reducedState.list[0]).toBe(testItem)
     })
 
     it('should return the correct state for action type ' + actionTypes.SEARCH_INDEX_ITEM, () => {
-        const state = {type: actionTypes.SEARCH_INDEX_ITEM}
+        const state = {}
+        const action = {type: actionTypes.SEARCH_INDEX_ITEM}
 
-        const reducedState = reducer(state, {})
+        const reducedState = reducer(state, action)
 
         expect(reducedState).toBe(state)
     })
