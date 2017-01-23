d40ae5d48611f7db97f0692e8d2ef08ca931abc5
beac0n <mcschempp@gmx.de>
2017-01-23 12:43:38 +0100

    fix tests

--- src/components/AddInput.test.js
+++ src/components/AddInput.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddInput from './AddInput';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {AddInput} from './AddInput'
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<AddInput />, div);
+    const div = document.createElement('div')
+    ReactDOM.render(<AddInput />, div)
 });
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+import App from './App'
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const initialState = {}
+    const store = mockStore(initialState)
+
+    const div = document.createElement('div')
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div)
 });
