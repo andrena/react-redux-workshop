53f3d1896dfbccfe62e3c0309b562a8aaafbc713
beac0n <mcschempp@gmx.de>
2017-01-23 13:27:45 +0100

    update tests

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -10,7 +10,7 @@ describe('reducer', () => {
         expect(reducedState).toBe(state)
     })
 
-    it('should return the correct state for action type ' + actionTypes.ADD_INDEX_ITEM, () => {
+    it('should update the list correctly for one item', () => {
         const testItem = 'TestItem';
         const action = {type: actionTypes.ADD_INDEX_ITEM, name: testItem}
 
@@ -18,6 +18,21 @@ describe('reducer', () => {
 
         expect(reducedState.list.length).toBe(1)
         expect(reducedState.list[0]).toBe(testItem)
+    })
+
+    it('should update the list correctly for 2 items', () => {
+        const testItem0 = 'TestItem0';
+        const testItem1 = 'TestItem1'
+
+        const action0 = {type: actionTypes.ADD_INDEX_ITEM, name: testItem0}
+        const action1 = {type: actionTypes.ADD_INDEX_ITEM, name: testItem1}
+
+        const reducedState0 = reducer(undefined, action0)
+        const reducedState1 = reducer(reducedState0, action1)
+
+        expect(reducedState1.list.length).toBe(2)
+        expect(reducedState1.list[0]).toBe(testItem0)
+        expect(reducedState1.list[1]).toBe(testItem1)
     })
 
     it('should return the correct state for action type ' + actionTypes.SEARCH_INDEX_ITEM, () => {
